a1a6bfe10ececac7970841bfc3a427f04439c0c3
serge160889 <serge_voit_2006@mail.ru>
2021-02-16 23:05:34 +0300

    item 1-5+HashSet

---
+++ src/academy/belhard/HashSet.java
@@ -0,0 +1,20 @@
+package academy.belhard;
+
+import java.util.Set;
+
+public class HashSet {
+    public static void main(String[] args) {
+       Person s1=new Person("Sergei","Voitehovich",Gender.MALE);
+       Person s2=new Person("Sergei","Voitehovich",Gender.MALE);
+       Person s3=new Person("Ylia","Ryabova",Gender.FEMALE);
+       Person s4=new Person("Ylia","Ryabova",Gender.FEMALE);
+       Person s5=new Person("Tom","Hardy",Gender.MALE);
+       Set<Person> myList =new java.util.HashSet<>();
+       myList.add(s1);
+       myList.add(s2);
+       myList.add(s3);
+       myList.add(s4);
+       myList.add(s5);
+        System.out.println("Number of different people="+myList.size());
+    }
+}
