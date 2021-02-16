c1cf1bb115d5997cc31ddf0079f9d5e5aa996d82
serge160889 <serge_voit_2006@mail.ru>
2021-02-16 21:26:40 +0300

    item 1-5

--- src/academy/belhard/Main.java
+++ src/academy/belhard/Main.java
@@ -3,6 +3,14 @@ package academy.belhard;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Person person1=new Person("Sergei","Voitehovich",Gender.MALE);
+	Person person2=new Person("Ann","Voitehovich",Gender.FEMALE);
+	boolean x1=person1.equals(person2);
+        System.out.println(x1);
+        int c1=person1.hashCode();
+        int c2=person2.hashCode();
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(person1);
     }
 }
---
+++ src/academy/belhard/Person.java
@@ -0,0 +1,39 @@
+package academy.belhard;
+
+import java.util.Objects;
+
+final class  Person {
+    private final String firstName;
+    private final String lastName;
+    private final Gender gender;
+
+    public Person(String firstName, String lastName, Gender gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object path){
+        if (this==path) return true;
+        if (!(path instanceof Person)) return false;
+        Person person=(Person) path;
+        return firstName == person.firstName &&
+                lastName == person.lastName &&
+                gender==person.gender;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName+lastName+gender);
+    }
+
+    @Override
+    public String toString(){
+        return "Person{"+
+                "firstName="+firstName+
+                ",lastName="+lastName+
+                ", gender="+gender+
+                '}';
+    }
+}
